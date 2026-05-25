9ead56d537c6c60ad43111011294d6a0973f64af
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-05-25 14:06:52 +0530

    Create Spring Boot Project

---
+++ src/test/java/org/example/firepredictionsystem/FirePredictionSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.firepredictionsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirePredictionSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
