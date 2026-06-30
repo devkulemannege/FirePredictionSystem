e05eda0dd046b2b0933036ccc1d17f03e753e246
Devpriya Kulemannege <80089198+devkulemannege@users.noreply.github.com>
2026-06-30 21:37:52 +0530

    Update TaskHandler.java
    remove temporary comments

--- src/main/java/org/example/firepredictionsystem/TaskHandler.java
+++ src/main/java/org/example/firepredictionsystem/TaskHandler.java
@@ -16,51 +16,51 @@ public class TaskHandler {
 
     public String submit_task(HttpSession session, String api) throws ParseException {
         // ---- dummy return for testing. TODO: remove for production ----
-        return """
-            {"status": "pending", "task_id": "dummy-task-id-12345"}
-            """;
+//        return """
+//            {"status": "pending", "task_id": "dummy-id-123"}
+//            """;
         // -------------------------------------------------------------
 
 
         /* use prepared session variables to construct json payload
          * send POST request to AppEEARS */
-//        String taskReply;
-//
-//        // format and prepare JSON payload
-//        String taskPayload = String.format("""
-//            {"params": {"coordinates": [{"latitude": "%s",
-//                        "longitude": "%s"}],
-//                "dates": [{"endDate": "%s", "startDate": "%s"}],
-//                "layers": [{"layer": "LST_Day_1km", "product": "MOD11A2.061"},
-//            {"layer": "_1_km_16_days_NDVI", "product": "MOD13A2.061"},
-//            {"layer": "sur_refl_b05", "product": "MOD09A1.061"}]},
-//                "task_name": "%s",
-//                "task_type": "point"}
-//            """,session.getAttribute("latitude"),
-//                session.getAttribute("longitude"),
-//                session.getAttribute("endDate"),
-//                session.getAttribute("startDate"),
-//                session.getAttribute("taskName"));
-//
-//        Object tokenObj = new JSONParser().parse(session.getAttribute("token").toString());
-//        JSONObject tokenInJson = (JSONObject) tokenObj; // convert String to json objects
-//
-//        try {
-//            taskReply = client.post()
-//                    .uri(String.format("%stask", api))
-//                    .headers(httpHeaders -> {
-//                        httpHeaders.setBearerAuth(tokenInJson.get("token").toString()); // use auth-token intsead of usr & psw
-//                        httpHeaders.set("content-type", "application/json");
-//                    })
-//                    .body(taskPayload)
-//                    .retrieve()
-//                    .body(String.class);
-//        } catch (HttpClientErrorException e) {
-//            System.out.printf("%s | %s%n", session.getAttribute("username").toString(), e.getMessage());
-//            session.invalidate(); // clear session variables to allow new values to be assigned
-//            taskReply = "";
-//        }
-//
-//        return taskReply;
+        String taskReply;
+
+        // format and prepare JSON payload
+        String taskPayload = String.format("""
+            {"params": {"coordinates": [{"latitude": "%s",
+                        "longitude": "%s"}],
+                "dates": [{"endDate": "%s", "startDate": "%s"}],
+                "layers": [{"layer": "LST_Day_1km", "product": "MOD11A2.061"},
+            {"layer": "_1_km_16_days_NDVI", "product": "MOD13A2.061"},
+            {"layer": "sur_refl_b05", "product": "MOD09A1.061"}]},
+                "task_name": "%s",
+                "task_type": "point"}
+            """,session.getAttribute("latitude"),
+                session.getAttribute("longitude"),
+                session.getAttribute("endDate"),
+                session.getAttribute("startDate"),
+                session.getAttribute("taskName"));
+
+        Object tokenObj = new JSONParser().parse(session.getAttribute("token").toString());
+        JSONObject tokenInJson = (JSONObject) tokenObj; // convert String to json objects
+
+        try {
+            taskReply = client.post()
+                    .uri(String.format("%stask", api))
+                    .headers(httpHeaders -> {
+                        httpHeaders.setBearerAuth(tokenInJson.get("token").toString()); // use auth-token intsead of usr & psw
+                        httpHeaders.set("content-type", "application/json");
+                    })
+                    .body(taskPayload)
+                    .retrieve()
+                    .body(String.class);
+        } catch (HttpClientErrorException e) {
+            System.out.printf("%s | %s%n", session.getAttribute("username").toString(), e.getMessage());
+            session.invalidate(); // clear session variables to allow new values to be assigned
+            taskReply = "";
+        }
+
+        return taskReply;
     }
 }
